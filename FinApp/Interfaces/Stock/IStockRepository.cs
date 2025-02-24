78c457a38a182488e4da8d32a013c80630f88814
IvesJc-Evo <ives.chiba@evosystems.com.br>
2025-02-24 20:21:09 -0300

    feat StockRepository and IStockRepository

---
+++ FinApp/Interfaces/Stock/IStockRepository.cs
@@ -0,0 +1,13 @@
+﻿using FinApp.DTOs.Stock;
+using FinApp.Models;
+
+namespace FinApp.Interfaces.Stock;
+
+public interface IStockRepository
+{
+    Task<List<StockModel>> GetAllStocksAsync();
+    Task<StockModel?> GetStockByIdAsync(Guid id);
+    Task<StockModel> CreateStockAsync(CreateStockRequestDto createStockRequestDto);
+    Task<StockModel?> UpdateStockByIdAsync(Guid id, UpdateStockRequestDto updateStockRequestDto);
+    Task<StockModel?> DeleteStockByIdAsync(Guid id);
+}
---
+++ FinApp/Repository/Stock/StockRepository.cs
@@ -0,0 +1,70 @@
+﻿
+using FinApp.Data;
+using FinApp.DTOs.Stock;
+using FinApp.Interfaces.Stock;
+using FinApp.Mappers.Stock;
+using FinApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinApp.Repository.Stock;
+
+public class StockRepository(AppDBContext context) : IStockRepository
+{
+    public async Task<List<StockModel>> GetAllStocksAsync()
+    {
+        var stocks = await context.Stocks.Select(s => s.ToStockDto()).ToListAsync();
+
+        return stocks.Select(x => x.ToStockModel()).ToList();
+    }
+
+    public async Task<StockModel?> GetStockByIdAsync(Guid id)
+    {
+        var stockModel = await context.Stocks.FindAsync(id);
+        var stockDto = stockModel?.ToStockDto();
+        return stockDto?.ToStockModel();
+    }
+
+    public async Task<StockModel> CreateStockAsync(CreateStockRequestDto createStockRequestDto)
+    {
+        var stockModel = createStockRequestDto.ToStockFromCreateDto();
+        await context.Stocks.AddAsync(stockModel);
+        await context.SaveChangesAsync();
+        return stockModel;
+
+    }
+
+    public async Task<StockModel?> UpdateStockByIdAsync(Guid id, UpdateStockRequestDto updateStockRequestDto)
+    {
+        var stockModel = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+
+        if (stockModel != null)
+        {
+            stockModel.Symbol = updateStockRequestDto.Symbol;
+            stockModel.CompanyName = updateStockRequestDto.CompanyName;
+            stockModel.Industry = updateStockRequestDto.Industry;
+            stockModel.Purchase = updateStockRequestDto.Purchase;
+            stockModel.LastDiv = updateStockRequestDto.LastDiv;
+            stockModel.MarketCap = updateStockRequestDto.MarketCap;
+        }
+        else
+        {
+            return null;
+        }
+
+        await context.SaveChangesAsync();
+        return stockModel;
+    }
+
+    public async Task<StockModel?> DeleteStockByIdAsync(Guid id)
+    {
+        var stockModel = await context.Stocks.FirstOrDefaultAsync(s => s.Id == id);
+        if (stockModel == null)
+        {
+            return null;
+        }
+
+        context.Stocks.Remove(stockModel);
+        await context.SaveChangesAsync();
+        return stockModel;
+    }
+}
