22565ed6ddf3b11155cdbc6c23b9c3ec3bb98513
Ives Chiba <ivesjundi@gmail.com>
2025-03-04 19:56:07 -0300

    feat data validation in Comments DTOs

--- FinApp/DTOs/Comments/CreateCommentDto.cs
+++ FinApp/DTOs/Comments/CreateCommentDto.cs
@@ -1,10 +1,16 @@
-﻿namespace FinApp.DTOs.Comment;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinApp.DTOs.Comments;
 
 public class CreateCommentDto
 {
+    [Required]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters long")]
+    [MaxLength(100, ErrorMessage = "Title must be no more than 100 characters")]
     public string Title { get; set; } = string.Empty;
     
+    [Required]
+    [MinLength(5, ErrorMessage = "Content must be at least 5 characters long")]
+    [MaxLength(500, ErrorMessage = "Content must be no more than 500 characters")]
     public string Content { get; set; } = string.Empty;
-
-
 }
--- FinApp/DTOs/Comments/UpdateCommentDto.cs
+++ FinApp/DTOs/Comments/UpdateCommentDto.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using FinApp.Models;
 
-namespace FinApp.DTOs.Comment;
+namespace FinApp.DTOs.Comments;
 
 public class UpdateCommentDto
 {
+    [Required]
+    [MinLength(5, ErrorMessage = "Title must be at least 5 characters long")]
+    [MaxLength(100, ErrorMessage = "Title must be no more than 100 characters")]
     public string Title { get; set; } = string.Empty;
     
+    [Required]
+    [MinLength(5, ErrorMessage = "Content must be at least 5 characters long")]
+    [MaxLength(500, ErrorMessage = "Content must be no more than 500 characters")]
     public string Content { get; set; } = string.Empty;
     
 }
